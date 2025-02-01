46a0bbd67bdff6638288228bed0140a9a9cdc7b9
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-02-01 11:23:41 +0100

    Exercice 2: update Context et Internationalisation

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -7,7 +7,6 @@ const ProductSearch = () => {
   const { isDarkTheme } = useContext(ThemeContext);
   // TODO: Exercice 2.1 - Utiliser le LanguageContext
   const { language, translations } = useContext(LanguageContext);
-  
   // TODO: Exercice 1.2 - Utiliser le hook useDebounce
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
@@ -24,7 +23,7 @@ const ProductSearch = () => {
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Rechercher un produit..."
+        placeholder={translations[language]?.searchPlaceholder || "Rechercher un produit..."}
         className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
       />
     </div>
